de137473365a79faf147100eb68c8555004b9433
Melvin Sembrano <melv@hey.com>
2026-07-20 14:40:45 +1000

    bump version for release

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,4 +4,4 @@ package version
 
 // Version is terman's current version. Bump this manually when cutting a
 // release; there is no build-time injection or git-tag automation.
-const Version = "0.6.3"
+const Version = "0.7.0"
